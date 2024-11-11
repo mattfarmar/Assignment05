7b4da1860200300a392e23cabd3a4db2c9d957aa
lilydao11 <ldao@olin.edu>
2024-11-10 19:13:10 -0500

    work on linearization, error with matrix dimensions for v_eq

---
+++ linearization.m
@@ -0,0 +1,30 @@
+%% Equilibrium
+box_params = struct();
+box_params.m = 5;
+box_params.I = 10;
+box_params.k_list = [3 3 3 3];
+box_params.l0_list = [4 4 4 4];
+box_params.P_world = [0 0 5 5 0; 0 5 5 0 0];
+box_params.P_box = [2 2 4 4 2; 2 4 4 2 2];
+box_params.g = -9.81;
+t_in = 0;
+
+my_rate_func = @(V_in) box_rate_func(t_in,V_in,box_params);
+
+V_eq = multi_newton_solver(my_rate_func, [1; 1; 0; 0; 0; 0], true);
+
+%% Linearization
+J_approx = approximate_jacobian(V_eq);
+
+my_linear_rate = @(t_in,V_in) J_approx*(V_in-V_eq);
+
+%% Modal Analysis
+U_mode = %your code here (use eig)
+omega_n = %your code here (use eig)
+%small number
+epsilon = %your code here
+V0 = Veq + epsilon*[Umode;0;0;0];
+tspan = %your code here
+%run the integration of nonlinear system
+% [tlist_nonlinear,Vlist_nonlinear] =...
+% your_integrator(my_rate_func,tspan,V0,...);
